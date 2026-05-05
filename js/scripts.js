91b133f7311289d3dfa946f1d0e69e86c995bcfc
Shevila Damayanti <144654408+sevill32@users.noreply.github.com>
2026-05-05 15:55:39 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,1 @@
-function scrollCertificates(amount) {
-    const slider = document.getElementById("certificateSlider");
-    slider.scrollBy({
-        left: amount,
-        behavior: "smooth"
-    });
-}
+
